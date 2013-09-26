85ac4aa3610e3f2f79c8af83eacd0b3a1fc33ce9
Derek M. Anderson <derek.anderson@coxinc.com>
2013-09-26 14:39:55 -0500

    Yobidashi initial commit

---
+++ src/yobidashi.js
@@ -0,0 +1,50 @@
+var yobidashi = (function () {
+    var _ = {
+        pub: function (eventname) {
+            //emit event
+            var uniqueName = 'yobidashi.' + eventname;
+            var CustomEvent = function (event, params) {
+                params = params || {
+                    bubbles: false,
+                    cancelable: false,
+                    detail: undefined
+                };
+                var evt = document.createEvent('CustomEvent');
+                evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+                return evt;
+            };
+
+            if (window.CustomEvent) {
+                var event = new CustomEvent(uniqueName, {
+                    'detail': ''
+                });
+                var elem = document.getElementsByTagName('body')[0];
+                elem.dispatchEvent(event);
+            } else {
+                if (document.attachEvent) {
+                    document.documentElement[uniqueName] += 1;
+                }
+            }
+        },
+        sub: function (eventname, callback) {
+            // bind a callback to an event emitter
+            if (document.createEvent) {
+                var obj = document.getElementsByTagName('body')[0];
+                obj.addEventListener('yobidashi.' + eventname, callback);
+            } else if (document.createEventObject) {
+                document.documentElement.attachEvent('onpropertychange', callback);
+            }
+        },
+        unsub: function (eventname, callback) {
+            if (document.createEvent) {
+                var obj = document.getElementsByTagName('body')[0];
+                obj.removeEventListener('yobidashi.' + eventname, callback);
+            } else if (document.createEventObject) {
+                document.documentElement.detachEvent('onpropertychange', callback);
+            }
+        }
+    };
+
+    return _;
+
+}())
