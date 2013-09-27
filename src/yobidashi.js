0e3d2025b741ffbd3aa12f8f24d2c4bdcb441260
Derek M. Anderson <derek.anderson@coxinc.com>
2013-09-26 20:36:27 -0500

    updated code comments

--- src/yobidashi.js
+++ src/yobidashi.js
@@ -1,8 +1,10 @@
 var yobidashi = {
         pub: function (eventname, data) {
             //emit event
+            //create unique eventname name
             var uniqueName = 'yobidashi.' + eventname;
             var CustomEvent = function (event, params) {
+                //a fill for IE9 and 10 proper operation
                 params = params || {
                     bubbles: false,
                     cancelable: false
@@ -12,12 +14,14 @@ var yobidashi = {
                 return evt;
             };
             if (window.CustomEvent) {
+                //check if we can use native eventing
                 var event = new CustomEvent(uniqueName, {
                     'detail': data
                 });
                 var elem = document.getElementsByTagName('body')[0];
                 elem.dispatchEvent(event);
             } else {
+                //use a document property event for eventing
                 if (document.attachEvent) {
                     document.documentElement[uniqueName] = data;
                 }
@@ -26,23 +30,27 @@ var yobidashi = {
         sub: function (eventname, callback) {
             // bind a callback to an event emitters
             if (document.createEvent) {
+                //bind an event to the body tag
                 var obj = document.getElementsByTagName('body')[0];
                 obj.addEventListener('yobidashi.' + eventname, callback);
             } else if (document.createEventObject) {
+                //attach a callback to the onpropertychange for eventing
                 document.documentElement.attachEvent('onpropertychange', callback);
             }
         },
         unsub: function (eventname, callback) {
             // remove bound event
             if (document.createEvent) {
+                //remove event from body tag
                 var obj = document.getElementsByTagName('body')[0];
                 obj.removeEventListener('yobidashi.' + eventname, callback);
             } else if (document.createEventObject) {
+                //detach event from onpropertychange event
                 document.documentElement.detachEvent('onpropertychange', callback);
             }
         },
         bind: function (func, context) {
-            //bind function ripped from underscore
+            //bind function ripped from underscore, changes the scope of this inside func to context
             var ArrayProto = Array.prototype,
                 FuncProto = Function.prototype,
                 slice = ArrayProto.slice,
